0d67c58f646c2d399e88cdbf3fdebaaf1e293fc7
Fenisu <su.izawa2@gmail.com>
2022-02-19 06:41:05 -0300

    Added option to target player and limit the shooting angle

--- Scenes/Scripts/Enemy.gd
+++ Scenes/Scripts/Enemy.gd
@@ -2,18 +2,27 @@ extends KinematicBody2D
 
 
 # Export variables
+export var shoot_angle = 360
 export var rotate_speed = 100
 export var shoot_timer_wait_time = 0.2
 export var spawnpoint_count = 4
 export var radius = 100
+export var bullet_speed = 100
+
 export var turns = false
 export var turn_timer_wait_time = 1
-export var bullet_speed = 100
+
+
+export var target_player = false
+
 
 # Nodes
 onready var shoot_timer = $ShootTimer
 onready var rotator = $Rotator
 onready var turn_timer = $TurnTimer
+
+onready var main = get_tree().root.get_node("Main")
+onready var player = main.get_node("Player")
 
 # Scenes
 var bullet_scene = preload("res://Scenes/Bullet.tscn")
@@ -24,8 +33,14 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	# BANANAS ROTAT E
-	var new_rotation = rotator.rotation_degrees + (rotate_speed * delta)
+	var new_rotation
+	if target_player:
+		# Targets player instead of rotating
+		new_rotation = rad2deg(player.global_position.angle_to_point(global_position))
+	else:
+		# BANANAS ROTAT E
+		new_rotation = rotator.rotation_degrees + (rotate_speed * delta)
+	
 	rotator.rotation_degrees = fmod(new_rotation, 360)
 
 
@@ -44,14 +59,14 @@ func update():
 		child.queue_free()
 	
 	# Gets the angle inbetween the spawnpoints
-	var step = (2 * PI) / spawnpoint_count
+	var step = deg2rad(shoot_angle) / spawnpoint_count
 	
 	# For every spawnpoint, create a new position node at the appropriate position and with the appropriate rotation
 	for i in range(spawnpoint_count):
 		var spawnpoint = Position2D.new()
 		
 		# Creates a vector with the distance of radius and rotates it by the step times i
-		var pos = Vector2(radius, 0).rotated(step * i)
+		var pos = Vector2(radius, 0).rotated(step * (i - (spawnpoint_count / 2)))
 		spawnpoint.position = pos
 		spawnpoint.rotation = pos.angle()
 		rotator.add_child(spawnpoint)
