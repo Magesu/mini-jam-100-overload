d906e17529e5374a3fea460f2097e99e00ee95aa
Ceccro <pppceccon@yahoo.com.br>
2022-02-21 01:24:32 -0300

    Cucumbers RISE!!!

--- Scenes/Scripts/Bullet.gd
+++ Scenes/Scripts/Bullet.gd
@@ -12,6 +12,7 @@ var velocity = Vector2.ZERO
 func _ready():
 	# Gets the direction of the bullet with the power of MATH
 	direction = Vector2(cos(get_rotation()),sin(get_rotation()))
+	z_index = 1
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
